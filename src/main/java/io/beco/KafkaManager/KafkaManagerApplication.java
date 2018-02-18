8d66dba208b2a2540e08ffb5b75ec0744664be8d
Jeffrey Zampieron <jzampieron@zproject.net>
2018-02-18 13:50:15 -0500

    Initial Commit from Spring Initializer

---
+++ src/main/java/io/beco/KafkaManager/KafkaManagerApplication.java
@@ -0,0 +1,12 @@
+package io.beco.KafkaManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaManagerApplication.class, args);
+	}
+}
