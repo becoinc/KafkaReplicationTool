c123af37daf5ef8678fea5c17ad12a9dec5315c8
Jeffrey Zampieron <jeff@beco.io>
2018-02-18 15:09:25 -0500

    Enable Prometheus metrics.

--- src/main/java/io/beco/KafkaManager/KafkaManagerApplication.java
+++ src/main/java/io/beco/KafkaManager/KafkaManagerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaManagerApplication {
-
-	public static void main(String[] args) {
+public class KafkaManagerApplication
+{
+    public static void main(String[] args) {
 		SpringApplication.run(KafkaManagerApplication.class, args);
 	}
 }
