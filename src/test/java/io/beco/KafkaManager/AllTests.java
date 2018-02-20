bfd64b4d3758e537bda05c8ded653232e8408d75
Jeffrey Zampieron <jzampieron@zproject.net>
2018-02-20 14:48:23 -0500

    Fix command line build and unit test

---
+++ src/test/java/io/beco/KafkaManager/AllTests.java
@@ -0,0 +1,29 @@
+/**
+ * @file AllTests.java
+ * <p>
+ * Copyright (C) 2018 by Beco Inc. All Rights Reserved.
+ * See included LICENSE file with this for terms of use.
+ * <p>
+ * Any use, compilation, or distribution of this source code constitutes consent to the
+ * terms and conditions in the license file.
+ * @date 2/20/18 2:46 PM
+ * @author jzampieron
+ */
+
+package io.beco.KafkaManager;
+
+/**
+ * AllTests is a class that does...
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith( Suite.class )
+@Suite.SuiteClasses(
+    {
+        KafkaManagerApplicationTests.class
+    } )
+public class AllTests
+{
+}
